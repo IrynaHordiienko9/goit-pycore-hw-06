b5e1aff862b67afee4435a00e862a09ae11b983d
Iryna Hordiienko <irynagordiyenko9@gmail.com>
2025-10-31 13:55:09 +0200

    hw 9.py

---
+++ hw 9.py
@@ -0,0 +1,66 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    def __init__(self, value):
+        if not value:
+            raise ValueError("Ім'я не може бути порожнім")
+        super().__init__(value)
+
+class Phone(Field):
+    def __init__(self, value):
+        if not value.isdigit() or len(value) != 10:
+            raise ValueError("Телефон має містити рівно 10 цифр")
+        super().__init__(value)
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                self.phones.remove(p)
+                return True
+        return False
+
+    def edit_phone(self, old_phone, new_phone):
+        for idx, p in enumerate(self.phones):
+            if p.value == old_phone:
+                self.phones[idx] = Phone(new_phone)
+                return True
+        return False
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p.value
+        return None
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+            return True
+        return False
+    
+    
